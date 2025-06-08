8ed4ef70a388d23e661eac5ac31b200c3df784bd
Atomarnez <53262816+barnes21bb@users.noreply.github.com>
2025-06-07 21:22:52 -0500

    Revert "Refactor app into modules"

--- src/utils.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-def validate_email(email: str) -> bool:
-    """Basic email validation."""
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return re.match(pattern, email) is not None
